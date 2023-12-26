0bc697e441f3cbafc72c91c3830c946c77344bcc
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-12-27 00:11:39 +0100

    clean up

--- app/src/main/java/com/example/bouddicaclient/ui/screens/activity/MainActivity.kt
+++ app/src/main/java/com/example/bouddicaclient/ui/screens/activity/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 
-import androidx.compose.foundation.isSystemInDarkTheme
-
 import com.example.bouddicaclient.ui.screens.mainscreen.MainScreen
 import com.example.bouddicaclient.ui.theme.HiltMVVMComposeMovieTheme
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 import dagger.hilt.android.AndroidEntryPoint
 
